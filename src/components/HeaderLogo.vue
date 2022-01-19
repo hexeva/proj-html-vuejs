53f59988cbff0c5874c54b663871271533a0a9c6
hexeva <patrick79.g@gmail.com>
2022-01-19 08:18:52 +0100

    end logo

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>LOGO</div>
+
+<div class="nexgen">
+     <div class="logo-wrapper">
+        <div class="nex">nex</div>
+        <div class="gen">gen</div>
+    </div>
+</div>
+   
 </template>
 
 <script>
@@ -9,5 +16,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
+
+.nexgen{
+    width: 150px;
+    height: 50px;
+    text-transform: uppercase;
+    font-size: 20px;
+    font-weight: 700;
+    letter-spacing: 5px;
+    
+    .logo-wrapper{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .nex{
+            width:50%;
+            height: 100%;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            color: $brand_logo_text;
+            background-color:$brand_logo_background;
+            filter:opacity(0.7);
+            border-top-left-radius:50%;
+            border-bottom-left-radius:50%;
+           
+        }
+
+        .gen{
+            width: 50%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            color: $brand_logo_variant;
+        }
+    }
+}
+
+
 
 </style>
